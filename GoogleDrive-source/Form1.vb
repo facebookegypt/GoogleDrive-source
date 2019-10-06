b04156e751a0b5bfd4e94db3f4656a9c062dcdd8
facebookegypt <evry1_falls@yahoo.com>
2019-10-06 23:37:07 +0200

    Creating new repo After finishing cleaning the source code from personal information

---
+++ GoogleDrive-source/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub TextBox1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles TextBox1.MouseDoubleClick
+        Dim OFD As New OpenFileDialog
+        With OFD
+            .Filter = ("Microsoft Access Database 2007/2010 (*.accdb)|*.accdb")
+            If .ShowDialog = DialogResult.OK Then
+                TextBox1.Text = .FileName
+            End If
+        End With
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim N As String = Main(TextBox1)
+        'TextBox1.Text = Environment.
+        If String.IsNullOrEmpty(TextBox1.Text) Then
+            '           MsgBox("Pick File.")
+            '          Exit Sub
+        End If
+        'CreateService()
+        'UploadFile(TextBox1.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ GoogleDrive-source/Module1.vb
@@ -0,0 +1,60 @@
+﻿''''https://adonetaccess2003.blogspot.com
+Imports Google.Apis.Auth.OAuth2
+Imports Google.Apis.Drive.v3
+Imports Google.Apis.Drive.v3.Data
+Imports Google.Apis.Services
+Imports Google.Apis.Util.Store
+Imports System.IO
+Imports System.Threading
+'Save "credentials.json" in your project directory (i.e D:\Google-Drive-example\)
+'Refresh 'Solution explorer', show all files, include "credentials.json" into your project
+'From "Properties panel" change "Copy to Output Directory" to "Copy Always".
+Module Module1
+    'If modifying these scopes, delete your previously saved credentials
+    'at ~/.credentials/drive-dotnet-quickstart.json
+    Dim Scopes() As String = {DriveService.Scope.DriveReadonly}
+    Dim ApplicationName As String = "Quickstart"
+    Private Service As DriveService = New DriveService
+    Public Function Main(textbox As TextBox) As String
+        Dim credential As UserCredential
+        'replace my credentials with yours, I will erase my credentaisl before I commit this to POST.
+        Using Stream = New FileStream("credentials.json", FileMode.Open, FileAccess.Read)
+            'The file token.json stores the user's access and refresh tokens, and is created
+            'automatically when the authorization flow completes for the first time.
+            Dim credPath As String = "token.json"
+            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(Stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    New FileDataStore(credPath, True)).Result
+            'Console.WriteLine("Credential file saved to: " + credPath)
+            textbox.Text = ("Credential file saved to: " + credPath + vbCrLf)
+        End Using
+        'Create Drive API service.
+        Dim Service = New DriveService(New BaseClientService.Initializer() With
+            {
+                .HttpClientInitializer = credential,
+                .ApplicationName = ApplicationName
+            })
+        ' Define parameters of request.
+        Dim listRequest As FilesResource.ListRequest = Service.Files.List()
+        listRequest.PageSize = 10
+        listRequest.Fields = "nextPageToken, files(id, name)"
+        'List files.
+        Dim files As IList(Of Data.File) = listRequest.Execute().Files
+        'Console.WriteLine("Files:")
+        textbox.Text += "files :" & vbCrLf
+        If (files IsNot Nothing And files.Count > 0) Then
+            For Each file In files
+                'Console.WriteLine("{0} ({1})", file.Name, file.Idf
+                textbox.Text += (file.Name & " : " & file.Id) & vbCrLf
+            Next
+        Else
+            'Console.WriteLine("No files found.")
+            textbox.Text = ("No files found.")
+        End If
+        'Console.Read()
+        Return textbox.Text
+    End Function
+End Module
